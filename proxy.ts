65f3910bc4b19e08329c199a73e59d2664a50e32
siyantech <siyantechglobal@gmail.com>
2026-07-31 22:25:34 +0530

    fix proxy.ts export function name

--- proxy.ts
+++ proxy.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 
-export function middleware(req: NextRequest) {
+export function proxy(req: NextRequest) {
   const { pathname } = req.nextUrl;
   if (pathname.startsWith('/admin') && pathname !== '/admin/login') {
     const adminAuth = req.cookies.get('admin_auth');
